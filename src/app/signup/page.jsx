15fe8193bff5daa369438b318f925e82b60c5ebd
semihzivali <zivalisemih@gmail.com>
2025-01-12 20:10:34 +0300

    adding api endpoint

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -43,7 +43,7 @@ export default function SignUpPage() {
 
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <input
-              className="p-2 mt-8 rounded-xl border text-black"
+              className="p-2 mt-2 rounded-xl border text-black" // mt-4 yerine mt-2
               type="text"
               name="name"
               placeholder="Full Name" // Yeni input
@@ -53,7 +53,7 @@ export default function SignUpPage() {
             />
 
             <input
-              className="p-2 mt-4 rounded-xl border text-black"
+              className="p-2 mt-2 rounded-xl border text-black" // mt-4 yerine mt-2
               type="email"
               name="email"
               placeholder="Email"
@@ -104,7 +104,7 @@ export default function SignUpPage() {
                 onClick={toggleConfirmPasswordVisibility}
               >
                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                <path d="M8 5.5a2.5 2.5 0=" />
+                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
               </svg>
             </div>
 
